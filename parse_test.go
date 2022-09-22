921022a953ac7aecad9b87cae0cef4b06da93bb4
Thomas Pham <phamtri2895@gmail.com>
2022-09-22 16:16:01 +0700

    update init (#9)

--- parse_test.go
+++ parse_test.go
@@ -4,7 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/klauspost/compress/zstd"
+	"io/fs"
 	"os"
+	"path/filepath"
+	"sync"
 	"testing"
 )
 
@@ -15,10 +18,83 @@ const (
 	//filePath = "./test_data/File 11636.dcm"
 	//filePath = "./test_data/File 32000"
 	//filePath = "./test_data/File 4000.dcm"
-	filePath = "./test_data/File 8000"
+	//filePath = "./test_data/File 8000"
 	//filePath = "./test_data/File 12000"
 	//filePath = "./test_data/File 160.dcm"
+	filePath = "/home/tripg/Downloads/1.dcm"
 )
+
+func readDir(dir string) ([]string, error) {
+	result := []string{}
+
+	visit := func(path string, f fs.DirEntry, err error) error {
+		if !f.IsDir() {
+			result = append(result, path)
+		}
+		return nil
+	}
+	err := filepath.WalkDir(dir, visit)
+	if err != nil {
+		return result, err
+	}
+	return result, nil
+}
+
+func TestStress(t *testing.T) {
+	dirPath := "/home/tripg/Documents/vietnhat/test/2022/04"
+	dicomPath, err := readDir(dirPath)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	var wg sync.WaitGroup
+	for _, pFile := range dicomPath {
+		wg.Add(1)
+		go func(pFile string) {
+			defer wg.Done()
+			file, err := os.Open(pFile)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			defer file.Close()
+			info, err := file.Stat()
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			fileSize := info.Size()
+
+			parser, err := NewParser(file, fileSize, false, false)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			err = parser.Parse()
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			var encoder, _ = zstd.NewWriter(nil, zstd.WithEncoderLevel(zstd.SpeedFastest))
+
+			mapTag := parser.Export(true)
+
+			b, err := json.Marshal(mapTag)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			compressed := encoder.EncodeAll(b, make([]byte, 0, len(b)))
+			fmt.Println("LEN BYTES AFTER", len(compressed))
+
+		}(pFile)
+
+		wg.Wait()
+	}
+
+}
 
 func TestNewParser(t *testing.T) {
 	file, err := os.Open(filePath)
