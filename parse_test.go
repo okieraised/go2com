1f89fbf6a9f2c3ff37081c2582cff94f03377815
tripg <phamtri2895@gmail.com>
2022-10-04 07:52:53 +0700

    update init tag

--- parse_test.go
+++ parse_test.go
@@ -21,7 +21,8 @@ const (
 	//filePath = "./test_data/File 8000"
 	//filePath = "./test_data/File 12000"
 	//filePath = "./test_data/File 160.dcm"
-	filePath = "/home/tripg/Downloads/1.dcm"
+	//filePath = "/home/tripg/Downloads/1.dcm"
+	filePath = "/home/tripg/Documents/dicom/hehe/1ee46cf8-e7a9587a-c2f49d1e-77f30744-570322cf.dcm"
 )
 
 func readDir(dir string) ([]string, error) {
@@ -38,6 +39,94 @@ func readDir(dir string) ([]string, error) {
 		return result, err
 	}
 	return result, nil
+}
+
+func TestNewParser2(t *testing.T) {
+	InitTagDict()
+	file, err := os.Open(filePath)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+	info, err := file.Stat()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fileSize := info.Size()
+
+	//------------------------------------------------------------------------------------------------------------------
+	parser, err := NewParser(file, fileSize, false, false)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	err = parser.Parse()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	val2, err := parser.GetElementByTagString("00280010")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("VAL2", val2)
+
+	mapTag := parser.Export(true)
+	//for key := range mapTag {
+	//	fmt.Println(key, mapTag[key])
+	//}
+
+	val, err := mapTag.GetElementByTagString("(0008,0008)")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("VAL", val)
+
+	//------------------------------------------------------------------------------------------------------------------
+	b, err := json.Marshal(mapTag)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("LEN BYTES BEFORE", len(b))
+
+	var encoder, _ = zstd.NewWriter(nil, zstd.WithEncoderLevel(zstd.SpeedFastest))
+
+	compressed := encoder.EncodeAll(b, make([]byte, 0, len(b)))
+	fmt.Println("LEN BYTES AFTER", len(compressed))
+
+	var decoder, _ = zstd.NewReader(nil, zstd.WithDecoderLowmem(true))
+	deflated, err := decoder.DecodeAll(compressed, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("LEN BYTES AFTER 2", len(deflated))
+
+	type dst map[string]interface{}
+	x := &dst{}
+	json.Unmarshal(deflated, x)
+	fmt.Println(x)
+
+	ds := parser.GetDataset()
+	val3, err := ds.FindElementByTagName("PixelData")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("VAL3", val3.ValueLength, len(val3.Value.([]byte)))
+
+	res4, err := ds.RetrieveFileUID()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("res4", res4)
 }
 
 func TestStress(t *testing.T) {
@@ -70,6 +159,7 @@ func TestStress(t *testing.T) {
 				fmt.Println(err)
 				return
 			}
+			InitTagDict()
 			err = parser.Parse()
 			if err != nil {
 				fmt.Println(err)
