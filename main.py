33505e37c82ce618bbc3c515c221ea5181d8db90
Thomas Pham <phamtri2895@gmail.com>
2022-10-08 07:36:42 +0700

    Hotfix/tag dict (#16)
    Update tag dict vr

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ fpath = "./test_data/File 8000"
 # fpath = "./test_data/File 12000"
 fpath = '/home/tripg/Documents/1.dcm'
 fpath = '/home/tripg/Downloads/mammo_dicoms/1.3.12.2.1107.5.12.7.5054.30000019090500530000000000556.dicom'
+fpath = '/home/tripg/Documents/dicom/test_data/400014'
+fpath = '/home/tripg/Documents/dicom/mammo_dicoms/1.2.840.113619.2.255.10452022879169.3670200508103440.2701.dicom'
 
 ds = dcmread(fpath)
 print(ds)
