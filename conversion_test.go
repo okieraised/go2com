ca0cd2188ce060f8d2e51396220114bbe16e0efb
Thomas Pham <phamtri2895@gmail.com>
2023-04-10 16:08:34 +0700

    Increase buffer (#32)

--- conversion_test.go
+++ conversion_test.go
@@ -2,6 +2,7 @@ package go2com
 
 import (
 	"fmt"
+	"github.com/okieraised/go2com/internal/utils"
 	"github.com/stretchr/testify/assert"
 	"os"
 	"testing"
@@ -25,4 +26,72 @@ func TestParser_Export(t *testing.T) {
 
 	mapTagVal := parser.Export(false)
 	fmt.Println(mapTagVal)
+}
+
+func TestParser_Buffer(t *testing.T) {
+	assert := assert.New(t)
+	InitTagDict()
+	file, err := os.Open("/home/tripg/workspace/dicom_buffer/D4CDC297.dcm")
+	assert.NoError(err)
+
+	defer file.Close()
+	info, err := file.Stat()
+	assert.NoError(err)
+	fileSize := info.Size()
+
+	parser, err := NewParser(file, fileSize, false, false)
+	assert.NoError(err)
+	err = parser.Parse()
+	assert.NoError(err)
+
+	mapTagVal := parser.Export(false)
+	fmt.Println(mapTagVal)
+}
+
+func TestParser_MultipleFiles(t *testing.T) {
+
+	assert := assert.New(t)
+
+	filePaths, err := utils.ReadDirRecursively("/home/tripg/workspace/dicom/test_data")
+	assert.NoError(err)
+	InitTagDict()
+	for _, fPath := range filePaths {
+		fmt.Println("process:", fPath)
+		file, err := os.Open(fPath)
+		assert.NoError(err)
+
+		defer file.Close()
+		info, err := file.Stat()
+		assert.NoError(err)
+		fileSize := info.Size()
+
+		parser, err := NewParser(file, fileSize, false, false)
+		assert.NoError(err)
+		err = parser.Parse()
+		assert.NoError(err)
+	}
+}
+
+func TestParser_MultipleFiles_2(t *testing.T) {
+
+	assert := assert.New(t)
+
+	filePaths, err := utils.ReadDirRecursively("/home/tripg/workspace/dicom/test_full")
+	assert.NoError(err)
+	InitTagDict()
+	for _, fPath := range filePaths {
+		fmt.Println("process:", fPath)
+		file, err := os.Open(fPath)
+		assert.NoError(err)
+
+		defer file.Close()
+		info, err := file.Stat()
+		assert.NoError(err)
+		fileSize := info.Size()
+
+		parser, err := NewParser(file, fileSize, false, false)
+		assert.NoError(err)
+		err = parser.Parse()
+		assert.NoError(err)
+	}
 }
